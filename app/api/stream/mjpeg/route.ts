f872f1e2adba204b90a5f77c8eb42eb35947f1ef
chunli <git@chunli.dev>
2024-10-24 06:04:53 -0700

    Update MJPEG stream URL to use host environment variable
    Signed-off-by: chunli <git@chunli.dev>

--- app/api/stream/mjpeg/route.ts
+++ app/api/stream/mjpeg/route.ts
@@ -1,6 +1,6 @@
 export async function GET() {
   const response = await fetch(
-    `${process.env.NEXT_PUBLIC_NANOKVM_URL}/api/stream/mjpeg`
+    `http://${process.env.NEXT_PUBLIC_NANOKVM_HOST}/api/stream/mjpeg`
   )
 
   const imageBuffer = await response.body
